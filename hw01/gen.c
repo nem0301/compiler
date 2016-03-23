092abac0e7a67d84e8993d93080ea99d8fcaa151
Kyusik Shin <sks156324@gmail.com>
2016-03-23 20:43:47 +0900

    add gen and deterministic finite automata

---
+++ hw01/gen.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NO_SYMBOL 3
+
+char alphabet[NO_SYMBOL] = { 'a', 'b', 'c' };
+
+void rec(char* str, int n){
+  int i;
+  int len = strlen(str);
+  char* temp = (char*) malloc (sizeof(char) * (len + 1));
+  
+  if ( n == 0) {
+    putchar('\n');
+  } else {
+    for (i = 0; i < NO_SYMBOL; i++){
+      sprintf(temp, "%s%c", str, alphabet[i]);
+      if ( n == 1) {
+          printf("%s", temp);
+      }
+      rec(temp, n-1);      
+    }    
+  }
+  free(temp);
+}
+
+int main(int argc, char** argv){
+  int n, i;
+  
+  if (argc < 2){
+    printf ("no input\n");
+    exit(1);
+  }
+  
+  n = atoi( argv[1] );
+
+  for (i = 0; i <=n; i++){
+    rec("", i);
+  }
+   
+  return 0;
+}
+
